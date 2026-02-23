78e8e36f8f251734b5b06c44deca3459f69fec83
Sirineelk <sirineelkamel@gmail.com>
2026-02-24 00:14:21 +0100

    Config Jenkins et test

--- src/test/java/org/sogeti/ecommerce/runners/TestRunner.java
+++ src/test/java/org/sogeti/ecommerce/runners/TestRunner.java
@@ -15,8 +15,8 @@ import org.sogeti.ecommerce.configuration.Hooks;
                 "org.sogeti.ecommerce.configuration"
         },
         plugin = { "pretty", "html:target/cucumber-reports.html","json:target/cucumber.json" },
-        monochrome = true,
-        tags = "@R6" //pour rajouter plusieurs tags il faut mettre un or entre eux
+        monochrome = true
+       // tags = "@R6" //pour rajouter plusieurs tags il faut mettre un or entre eux
 
 )
 public class TestRunner extends Hooks {
